0ed938a17dccd75fbce92da701b1f6a2da9b7032
kkamkou <kkamkou@gmail.com>
2012-02-15 11:06:12 +0300

    refactored

--- jquery.ebony.js
+++ jquery.ebony.js
@@ -24,56 +24,12 @@
 (function (doc, $) {
     "use strict";
 
-    var jqEbony = function () {
-        var layout = null,
-            callbackClose = null;
-    };
-
-    jqEbony.prototype = {
-        setCallbackClose: function (obj) {
-            this.callbackClose = obj;
-            return this;
-        },
-
-        setLayout: function (obj) {
-            this.layout = obj;
-            return this;
-        },
-
-        getLayout: function () {
-            return this.layout;
-        },
-
-        setIndexZ: function (val) {
-            $('body').data('jqEbony', val);
-            return this;
-        },
-
-        getIndexZ: function () {
-            return parseInt($('body').data('jqEbony'), 10);
-        },
-
-        close: function (speed) {
-            var $self = this;
-
-            // close callback
-            if (typeof (this.callbackClose) === 'function') {
-                this.callbackClose(this.getIndexZ() + 1);
-            }
-
-            // overlay close
-            this.getLayout().fadeOut(speed || 0, function () {
-                $self.setIndexZ($self.getIndexZ() - 1);
-                $self.getLayout().remove();
-            });
-
-            return this;
-        }
-    };
-
-    // jQuery plugin
-    $.fn.jqEbony = function (options) {
-        options = $.extend({
+    // main class
+    var JqEbony = function (options, element) {
+        // default members
+        this._layout = null;
+        this._element = element;
+        this._options = $.extend({
             'opacity': 0.5,
             'zIndex': 99999,
             'callbackClose': null,
@@ -81,62 +37,180 @@
             'animationSpeed': 0, // in ms, for example: 200, 400 or 800
             'color': '#000'
         }, options || {});
+    };
 
-        var $self = this,
-            $holder = $('<div />'),
-            theOverlay = new jqEbony(),
-            currentZ = (theOverlay.getIndexZ() || options.zIndex) + 1;
-
-        $holder.css({
-            'bottom': 0,
-            'display': 'none',
-            'left': 0,
-            'position': 'fixed',
-            '_position': 'absolute', // ie fix
-            'right': 0,
-            'top': 0,
-            'background-color' : options.color,
-            'z-index': currentZ,
-            'opacity': options.opacity,
-            'width': '100%',
-            'height': '100%'
-        });
-
-        // escape key
-        $(doc).one('keydown', function (event) {
-            if ((event.keyCode || event.which) === 27) {
-                event.stopImmediatePropagation();
-                return theOverlay.close(options.animationSpeed);
+    // extends
+    JqEbony.prototype = {
+        // returns the ebony layout
+        getLayout: function () {
+            if (!this._layout) {
+                this._layout = this._getDefaultLayout();
             }
-        });
+            return this._layout;
+        },
 
-        // let's make the current event first
-        $(doc).data('events').keydown.unshift(
-            $(doc).data('events').keydown.pop()
-        );
+        // returns z-index value
+        getIndexZ: function () {
+            return parseInt(
+                $('body').data('jqEbony') || this.getOptions().zIndex,
+                10
+            );
+        },
 
-        // DOM update
-        $('body').append(
-            $holder.bind('click', function () {
-                return theOverlay.close(options.animationSpeed);
-            })
-        );
+        // options here
+        getOptions: function () {
+            return this._options;
+        },
 
-        // creating object
-        theOverlay.setLayout($holder)
-            .setCallbackClose(options.callbackClose)
-            .setIndexZ(currentZ);
+        // current element
+        getElement: function () {
+            return this._element;
+        },
 
-        // we should display all we have so far
-        $holder.fadeIn(options.animationSpeed, function () {
-            $self.css('z-index', currentZ + 1)
-                .fadeIn(options.animationSpeed, function () {
-                    if (typeof (options.callbackOpen) === 'function') {
-                        options.callbackOpen();
+        // sets z-index value
+        setIndexZ: function (val) {
+            $('body').data('jqEbony', val);
+            return this;
+        },
+
+        open: function () {
+            // key binding
+            this._setListener();
+            this._transform();
+
+            // defaults
+            var that = this;
+
+            // we should display all we have so far
+            this.getLayout().fadeIn(
+                this.getOptions().animationSpeed,
+                function () {
+                    that.getElement()
+                        .css('z-index', that.getIndexZ() + 1)
+                        .fadeIn(
+                            that.getOptions().animationSpeed,
+                            function () {
+                                if (typeof (that.getOptions().callbackOpen) === 'function') {
+                                    that.getOptions().callbackOpen(that);
+                                }
+                            }
+                        );
+                }
+            );
+
+            return this;
+        },
+
+        close: function () {
+            var that = this;
+
+            // overlay close
+            this.getElement().fadeOut(
+                this.getOptions().animationSpeed || 0,
+                function () {
+                    // before close function
+                    if (typeof (that.getOptions().callbackClose) === 'function') {
+                        that.getOptions().callbackClose(that);
                     }
-                });
-        });
 
-        return theOverlay;
+                    // DOM cleanup
+                    that.getLayout().fadeOut(
+                        that.getOptions().animationSpeed * 0.5,
+                        function () {
+                            that._layoutRemove();
+                        }
+                    );
+                }
+            );
+
+            return this;
+        },
+
+        // default element with style (black area)
+        _getDefaultLayout: function () {
+            return $('<div />').css({
+                'bottom': 0,
+                'display': 'none',
+                'left': 0,
+                'position': 'fixed',
+                '_position': 'absolute', // ie fix
+                'right': 0,
+                'top': 0,
+                'background-color' : this.getOptions().color,
+                'z-index': this.getIndexZ(),
+                'opacity': this.getOptions().opacity,
+                'width': '100%',
+                'height': '100%'
+            });
+        },
+
+        // adds events (escape and the mouse click)
+        _setListener: function () {
+            var that = this;
+
+            // escape key
+            $(doc).one('keydown', function (event) {
+                if ((event.keyCode || event.which) === 27) {
+                    event.stopImmediatePropagation();
+                    return that.close(that.getOptions().animationSpeed);
+                }
+            });
+
+            // let's make the current event first
+            $(doc).data('events').keydown.unshift(
+                $(doc).data('events').keydown.pop()
+            );
+
+            // DOM update
+            $('body').append(
+                that.getLayout().bind('click', function () {
+                    return that.close(that.getOptions().animationSpeed);
+                })
+            );
+        },
+
+        // element transformation
+        _transform: function () {
+            var $elem = this.getElement();
+
+            // storing old styles
+            $elem.data('jqEbonyData', {
+                'position': $elem.css('position'),
+                'display': $elem.css('display'),
+                'visibility': $elem.css('visibility')
+            });
+
+            // position of the element
+            $elem.css(
+                'position',
+                $elem.css('position') === 'static' ? 'relative' : 'absolute'
+            );
+
+            return this;
+        },
+
+        // custom data cleanup
+        _revert: function () {
+            this.getElement()
+                .css(this.getElement().data('jqEbonyData'))
+                .removeData('jqEbonyData');
+            return this;
+        },
+
+        // removes black area from the DOM
+        _layoutRemove: function () {
+            this._revert()
+                .setIndexZ(this.getIndexZ() - 1)
+                .getLayout().remove();
+
+            this._layout = null; // memory cleanup
+
+            return this;
+        }
+    };
+
+    // jQuery plugin
+    $.fn.jqEbony = function (options) {
+        return (new JqEbony(options, this)).open();
     };
 }(document, jQuery));
